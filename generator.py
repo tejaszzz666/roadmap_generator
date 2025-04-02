b2f1eb6aef574c3a976c3ec5f4678347d61f6280
tejaszzz666 <tejassonawane6991@gmail.com>
2025-04-02 09:43:43 +0530

    generator.py

--- generator.py
+++ generator.py
@@ -13,7 +13,7 @@ hf_api_keys = [
 api_key_cycle = cycle(hf_api_keys)
 
 @lru_cache(maxsize=50)
-def get_hf_response(question, model_id="HuggingFaceH4/zephyr-7b-beta"):
+def get_hf_response(question, model_id="tiiuae/falcon-7b-instruct"):
     api_url = f"https://api-inference.huggingface.co/models/{model_id}"
 
     for _ in range(len(hf_api_keys)):
